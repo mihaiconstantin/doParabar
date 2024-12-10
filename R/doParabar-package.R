29c52dea2c3fa3974a0e4bb5f23d2c61eea1b61e
mihaiconstantin <mihai@mihaiconstantin.com>
2024-12-10 18:14:54 +0100

    Feat: add package file with imports and lifecycle hooks

---
+++ R/doParabar-package.R
@@ -0,0 +1,39 @@
+# # # # # # # # # # # # # # # # # # # # # # # # # # # #
+#                              _                      #
+#                             | |                     #
+#   _ __    __ _  _ __   __ _ | |__    __ _  _ __     #
+#  | '_ \  / _` || '__| / _` || '_ \  / _` || '__|    #
+#  | |_) || (_| || |   | (_| || |_) || (_| || |       #
+#  | .__/  \____||_|    \____||____/  \____||_|       #
+#  | |                                                #
+#  |_|                                                #
+#                                                     #
+# # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# Author: Mihai A. Constantin                         #
+# Documentation: https://parabar.mihaiconstantin.com  #
+# Contact: mihai@mihaiconstantin.com                  #
+# # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+
+# Imports.
+#' @importFrom parabar par_lapply
+#' @importFrom foreach setDoPar getResult
+#' @importFrom iterators iter
+
+#' @template doParabar
+"_PACKAGE"
+
+
+# On package load.
+.onLoad <- function(libname, pkgname) {
+}
+
+
+# On package attach.
+.onAttach <- function(libname, pkgname) {
+}
+
+
+# On package unload.
+.onUnload <- function(libpath) {
+}
