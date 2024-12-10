b00509b0ea6014de988e9751e40bc1d47bc64e94
mihaiconstantin <mihai@mihaiconstantin.com>
2024-12-10 18:15:09 +0100

    Docs: add preliminary package documentation

---
+++ man-roxygen/doParabar.R
@@ -0,0 +1,11 @@
+#' @title Foreach Parallel Adaptor For The `parabar` Package
+#'
+#' @details The [`doParabar::doParabar`] package acts as a [`foreach::foreach`]
+#' parallel adaptor for [`parabar::parabar`] backends. It provides a minimal
+#' implementation for the [`foreach::%dopar%`] operator, enabling seamless
+#' integration of the [`parabar::parabar`] package with the [`foreach::foreach`]
+#' package. For more details, please consult de documentation for
+#' [`parabar::parabar`] at
+#' [parabar.mihaiconstantin.com](https://parabar.mihaiconstantin.com).
+#'
+#' @keywords internal
