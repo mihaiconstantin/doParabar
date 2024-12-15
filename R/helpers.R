b65547019eb9fbd57e8bc106cd0a0e97a330fb8c
mihaiconstantin <mihai@mihaiconstantin.com>
2024-12-15 12:16:30 +0100

    Feat: add helper to get the registered `parabar` backend details

--- R/helpers.R
+++ R/helpers.R
@@ -1,4 +1,40 @@
 #' @title
+#' Registered Implementation Information
+#'
+#' @description
+#' This function is used internally by [`foreach::foreach`] to retrieve
+#' information about the registered [`foreach::%dopar%`] operator
+#' implementation. More specifically, this function is called by
+#' [`foreach::getDoParName()`], [`foreach::getDoParRegistered()`],
+#' [`foreach::getDoParVersion()`], and [`foreach::getDoParWorkers()`].
+#'
+#' @param data An object of class [`parabar::Backend`] representing the backend
+#' used for the [`foreach::%dopar%`] operator implementation.
+#'
+#' @param item A character string specifying the information item to be
+#' retrieved about the registered [`foreach::%dopar%`] operator implementation.
+#'
+#' @return The requested information item.
+#'
+#' @keywords internal
+parabarInfo <- function(data, item) {
+    switch(item,
+        # Get the number of backend workers.
+        workers = getNumberOfWorkers(data),
+
+        # Provide a name for the registered implementation.
+        name = paste0("doParabar (", parabar::Helper$get_class_name(data), ")"),
+
+        # Get the package version.
+        version = utils::packageDescription("doParabar", fields = "Version"),
+
+        # For any other informational items return `NULL`.
+        NULL
+    )
+}
+
+
+#' @title
 #' Get the Number of Workers
 #'
 #' @param backend An object of class [`parabar::Backend`] representing the
