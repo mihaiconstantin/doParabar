954683b7ae597f66c025334c43f7df25f1f2bde4
mihaiconstantin <mihai@mihaiconstantin.com>
2024-12-15 14:51:27 +0100

    Fix: update order of helper functions

--- R/helpers.R
+++ R/helpers.R
@@ -1,4 +1,33 @@
 #' @title
+#' Get the Number of Workers
+#'
+#' @param backend An object of class [`parabar::Backend`] representing the
+#' backend used for the [`foreach::%dopar%`] operator implementation.
+#'
+#' @return An integer representing the number of workers for the registered
+#' backend.
+#'
+#' @keywords internal
+getNumberOfWorkers <- function(backend) {
+    # Get the number of workers based on the backend type.
+    switch(parabar::Helper$get_class_name(backend),
+
+        # If we are dealing with an asynchronous backend.
+        "AsyncBackend" = backend$cluster$run(function() {
+            # Get the number of workers.
+            length(cluster)
+        }),
+
+        # If we are dealing with a synchronous backend.
+        "SyncBackend" = length(backend$cluster),
+
+        # Otherwise default to a single worker.
+        1
+    )
+}
+
+
+#' @title
 #' Registered Implementation Information
 #'
 #' @description
@@ -30,35 +59,6 @@ parabarInfo <- function(data, item) {
 
         # For any other informational items return `NULL`.
         NULL
-    )
-}
-
-
-#' @title
-#' Get the Number of Workers
-#'
-#' @param backend An object of class [`parabar::Backend`] representing the
-#' backend used for the [`foreach::%dopar%`] operator implementation.
-#'
-#' @return An integer representing the number of workers for the registered
-#' backend.
-#'
-#' @keywords internal
-getNumberOfWorkers <- function(backend) {
-    # Get the number of workers based on the backend type.
-    switch(parabar::Helper$get_class_name(backend),
-
-        # If we are dealing with an asynchronous backend.
-        "AsyncBackend" = backend$cluster$run(function() {
-            # Get the number of workers.
-            length(cluster)
-        }),
-
-        # If we are dealing with a synchronous backend.
-        "SyncBackend" = length(backend$cluster),
-
-        # Otherwise default to a single worker.
-        1
     )
 }
 
